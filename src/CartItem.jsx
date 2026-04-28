df17e771b03571878ff2d8bdeb935c104e86ba28
Centhor <vincenthizon266@gmail.com>
2026-04-28 15:40:27 +0800

    feat: cartItem methods defined

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,29 +8,32 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
-  };
+  const calculateTotalAmount = () => 
+    cart.reduce((total, item) => total + calculateTotalCost(item), 0)
 
-  const handleContinueShopping = (e) => {
-   
-  };
+  const handleContinueShopping = (e) => 
+    onContinueShopping(e);
 
-
-
-  const handleIncrement = (item) => {
-  };
+  const handleIncrement = (item) => 
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}))
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity>1){
+      dispatch(updateQuantity({name: item.name, quantity: item.quantity-1}))
+    }else{
+      handleRemove(item)
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}))
   };
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-  };
+  const calculateTotalCost = (item) => 
+    item.quantity * parseFloat(item.cost.substring(1))
+
+  const handleCheckoutShopping = (e) => alert('Functionality to be added for future reference')
 
   return (
     <div className="cart-container">
